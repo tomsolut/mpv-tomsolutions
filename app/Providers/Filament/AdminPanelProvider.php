ad414915b14cb9a0b62cda39ad80f163e64319b8
AshBarseghyan <ashbarseghyan99@gmail.com>
2025-04-11 14:08:13 +0400

    collapse sidebar

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->sidebarCollapsibleOnDesktop()
             ->id('admin')
             ->path('/')
             ->login()

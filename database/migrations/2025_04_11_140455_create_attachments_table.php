daab659ea72354e05d2578fa3e3db331dc4b99c4
AshBarseghyan <ashbarseghyan99@gmail.com>
2025-04-14 14:49:01 +0400

    fix attachments DB issue

--- database/migrations/2025_04_11_140455_create_attachments_table.php
+++ database/migrations/2025_04_11_140455_create_attachments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\DeviceType;
+use App\Models\DoctorDevice;
 
 return new class extends Migration {
     /**
@@ -13,7 +13,7 @@ return new class extends Migration {
     {
         Schema::create('attachments', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(DeviceType::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(DoctorDevice::class)->constrained()->cascadeOnDelete();
             $table->string('name')->nullable();
             $table->string('file');
             $table->timestamps();
